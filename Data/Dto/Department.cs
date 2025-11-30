eac16dbc612356e1a00bd32781edf5f9dd1fbcb0
Yaroslav <bortnikyaroslav@gmail.com>
2025-12-01 01:05:58 +0200

    fully done

---
+++ Data/Dto/Department.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_all.Data.Dto
+{
+    using csharp_all.Data.Attributes;
+
+    [TableName("ShopDepartments")]
+    internal class Department
+    {
+        public Guid Id { get; set; }
+        public String Name { get; set; } = null!;
+
+        public override string? ToString()
+        {
+            String id = Id.ToString();
+            return $"{id[..3]}...{id[^3..]} {Name}";
+        }
+    }
+}
